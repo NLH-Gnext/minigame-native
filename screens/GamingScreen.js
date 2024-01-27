023ca0b1c48d136a98b45a61ab93d499068f1b68
nyan <nyanlinhtet2244@gmail.com>
2024-01-27 11:07:09 +0630

    gamescreen

--- screens/GamingScreen.js
+++ screens/GamingScreen.js
@@ -1,6 +1,23 @@
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 const GamingScreen = () => {
-  return <Text>GamingScreen</Text>;
+  return (
+    <View>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+        <View>Log Round</View>
+      </View>
+    </View>
+  );
 };
 
 export default GamingScreen;
+
+const styles = StyleSheet.create({
+  screen : {
+    flex : 1,
+    padding : 1,
+  }
+})
