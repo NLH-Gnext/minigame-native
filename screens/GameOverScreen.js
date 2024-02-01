b7b2e14a6cafbbb79bb61a29d5846f24b4314e89
NLH-Gnext <gnext.nyanlinhtet@gmail.com>
2024-02-01 23:33:13 +0630

    gamescreen_done

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,30 +1,61 @@
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+  useWindowDimensions,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/Colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 const GameOverScreen = ({ roundNumber, userNumber, onStartNewGame }) => {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+  if (height < 420) {
+    imageSize = 80;
+  }
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>Game Over</Title>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={require("../assets/success.jpg")} />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>Game Over</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            style={styles.image}
+            source={require("../assets/success.jpg")}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          Your Phone needed <Text style={styles.highlight}>{roundNumber}</Text>{" "}
+          rounds to guess the number{" "}
+          <Text style={styles.highlight}>{userNumber}</Text>.
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your Phone needed <Text style={styles.highlight}>{roundNumber}</Text>{" "}
-        rounds to guess the number{" "}
-        <Text style={styles.highlight}>{userNumber}</Text>.
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 };
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get("window").width;
+// const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -32,9 +63,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
-    borderRadius: deviceWidth < 380 ? 75 : 150,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
+    // borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     margin: 36,
@@ -46,7 +77,8 @@ const styles = StyleSheet.create({
   },
   summaryText: {
     fontFamily: "OpenSans",
-    fontSize: deviceWidth < 380 ? 16 : 24,
+    // fontSize: deviceWidth < 380 ? 16 : 24,
+    fontSize: 24,
     textAlign: "center",
     marginVertical: 24,
   },
